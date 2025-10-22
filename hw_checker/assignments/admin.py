e68bb07f7a2554e92ff195392f5d1478dc269c92
Deruzhinsky Dmitry <91655905+terebellum@users.noreply.github.com>
2025-10-22 15:37:36 +0300

    Add linters to CI of github (#1)
    добавляет проверку
    
    * Тестов в проекте
    * Линтеров в проекте
    
    А также чинит существующие проблемы с линтерами

--- hw_checker/assignments/admin.py
+++ hw_checker/assignments/admin.py
@@ -1,13 +1,14 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.models import User
+
 from .models import Homework, Submission, UserProfile
 
 
 class UserProfileInline(admin.StackedInline):
     model = UserProfile
     can_delete = False
-    verbose_name_plural = 'Профиль'
+    verbose_name_plural = "Профиль"
 
 
 class UserAdmin(BaseUserAdmin):
@@ -22,26 +23,24 @@ admin.site.register(User, UserAdmin)
 @admin.register(Homework)
 class HomeworkAdmin(admin.ModelAdmin):
     """Админка для домашних заданий"""
-    list_display = ['title', 'due_date', 'created_at']
-    list_filter = ['created_at', 'due_date']
-    search_fields = ['title', 'description']
-    ordering = ['-created_at']
+
+    list_display = ["title", "due_date", "created_at"]
+    list_filter = ["created_at", "due_date"]
+    search_fields = ["title", "description"]
+    ordering = ["-created_at"]
 
 
 @admin.register(Submission)
 class SubmissionAdmin(admin.ModelAdmin):
     """Админка для отправок работ"""
-    list_display = ['homework', 'student', 'submitted_at', 'grade']
-    list_filter = ['submitted_at', 'grade', 'homework']
-    search_fields = ['student__username', 'homework__title']
-    ordering = ['-submitted_at']
-    readonly_fields = ['submitted_at']
-    
+
+    list_display = ["homework", "student", "submitted_at", "grade"]
+    list_filter = ["submitted_at", "grade", "homework"]
+    search_fields = ["student__username", "homework__title"]
+    ordering = ["-submitted_at"]
+    readonly_fields = ["submitted_at"]
+
     fieldsets = (
-        ('Информация о работе', {
-            'fields': ('homework', 'student', 'solution_file', 'submitted_at')
-        }),
-        ('Проверка', {
-            'fields': ('grade', 'feedback')
-        }),
+        ("Информация о работе", {"fields": ("homework", "student", "solution_file", "submitted_at")}),
+        ("Проверка", {"fields": ("grade", "feedback")}),
     )
